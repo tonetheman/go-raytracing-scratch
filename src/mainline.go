9e5044986bd2193fd7008b4284ab37460a8d8898
tony <tonetheman@gmail.com>
2016-01-28 08:33:39 -0600

    much more code

--- src/mainline.go
+++ src/mainline.go
@@ -47,6 +47,17 @@ type Sphere struct {
 	transparency, reflection float64
 }
 
+func (s *Sphere) init(c * Vec3, r float64, sc * Vec3, refl float64,
+	transp float64, ec * Vec3) {
+	s.center = *c;
+	s.radius = r;
+	s.radius2 = r * r;
+	s.surfaceColor = *sc;
+	s.emissionColor = *ec;
+	s.transparency = transp;
+	s.reflection = refl;
+}
+
 func (s *Sphere) intersect(rayorig * Vec3, 
 	raydir * Vec3, t0 *float64, t1 *float64) bool {
 	var l Vec3 = s.center.minus(rayorig);
@@ -59,22 +70,61 @@ func (s *Sphere) intersect(rayorig * Vec3,
 		return false;
 	}
 	var thc float64 = math.Sqrt(s.radius2 - d2);
-	// TODO: how do you do this in golang?
 	*t0 = tca - thc;
 	*t1 = tca + thc;
 	return true;
 }
 
+func render(spheres [6]Sphere) {
+	var width int = 640
+	var height int = 480;
+
+	var image []Vec3 = make([]Vec3,width*height);
+	fmt.Println(image)
+	var counter int = 0;
+	// var pixel Vec3 = image[counter]
+	var invWidth float64 = 1/float64(width);
+	var invHeight = 1 /float64(height);
+	var fov float64 = 30.0;
+	var aspectratio float64 = float64(width)/float64(height)
+	var angle = math.Tan(math.Pi * 0.5 * fov/180.0);
+
+	for y := 0; y < height; y++ {
+		for x := 0; x<width; x++ {
+			var xx float64 = (2 * ((float64(x) + 0.5) * invWidth) - 1) * 
+				angle * aspectratio;
+			var yy float64 = (1 - 2 * 
+				((float64(y) + 0.5) * invHeight)) * angle; 
+			var raydir Vec3 = Vec3{xx,yy,-1};
+			raydir.normalize();
+
+			counter++;
+		}
+	}
+
+	fmt.Println("P6");
+	fmt.Println(width,height);
+	fmt.Println(255)
+}
+
 func main() {
+	var spheres [6]Sphere
+	spheres[0] = Sphere{ Vec3{0.0,-10004,-20},10000,2*10000,
+		Vec3{0.20,0.20,0.20}, Vec3{0,0,0}, 0, 0.0  }
+	spheres[1] = Sphere{ Vec3{0.0,0,-20},4,2*4,
+		Vec3{1.0,0.32,0.36}, Vec3{0,0,0}, 1, 0.5 }
+	spheres[2] = Sphere{ Vec3{5,-1,-15}, 2, 2*2,
+		Vec3{0.9,0.76,0.46}, Vec3{0,0,0}, 1, 0.0 }
+	spheres[3] = Sphere{ Vec3{5,0,-25}, 3, 2*3,
+		Vec3{0.65,0.77,0.97}, Vec3{0,0,0}, 1, 0.0 }
+	spheres[4] = Sphere{ Vec3{-5.5,0,-15}, 3, 2*3,
+		Vec3{0.90,0.90,0.90}, Vec3{0,0,0}, 1, 0.0 }
 
-	var v Vec3
-	v.x = 1
-	v.y = 2
-	v.z = 3
-	
-	fmt.Println(v)
+	// light
+	spheres[5] = Sphere{ Vec3{0.0,20,-35}, 3, 2*3,
+		Vec3{0.0,0.0,0.0}, Vec3{3,3,3}, 0, 0.0 }
 
-	v.normalize()
+	render(spheres)
 
-	fmt.Println(v)
+
 }
